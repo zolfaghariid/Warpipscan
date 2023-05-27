3a103dac8bf754f400c976daba72c85d3092a4f8
RezaDorosti <reza.dorosti.1367@gmail.com>
2023-05-27 10:15:06 +0330

    Update warp-yxip.sh to @yebekhe version with github links

--- files/warp-yxip/warp-yxip.sh
+++ files/warp-yxip/warp-yxip.sh
@@ -5,6 +5,7 @@ GREEN="\033[32m"
 YELLOW="\033[33m"
 PLAIN='\033[0m'
 
+# Functions for text formatting
 red() {
     echo -e "\033[31m\033[01m$1\033[0m"
 }
@@ -17,41 +18,43 @@ yellow() {
     echo -e "\033[33m\033[01m$1\033[0m"
 }
 
+# Function to determine the CPU architecture
 archAffix(){
     case "$(uname -m)" in
         i386 | i686 ) echo '386' ;;
         x86_64 | amd64 ) echo 'amd64' ;;
         armv8 | arm64 | aarch64 ) echo 'arm64' ;;
         s390x ) echo 's390x' ;;
-        * ) red "不支持的CPU架构!" && exit 1 ;;
+        * ) red "Unsupported CPU architecture!" && exit 1 ;;
     esac
 }
 
+# Function to select the best WARP endpoint IP
 endpointyx(){    
-    # 下载优选工具软件，感谢某匿名网友的分享的优选工具
+    # Download the endpoint selection tool, thanks to an anonymous user for sharing
     wget https://github.com/RezaDorosti/Misaka_warp-script/raw/main/files/warp-yxip/warp-linux-$(archAffix) -O warp
-    
-    # 取消 Linux 自带的线程限制，以便生成优选 Endpoint IP
+    # Turn off the default thread limit of Linux, to enable optimal endpoint IP generation
     ulimit -n 102400
     
-    # 启动 WARP Endpoint IP 优选工具
+    # Start the WARP endpoint IP selection tool
     chmod +x warp && ./warp >/dev/null 2>&1
     
-    # 显示前十个优选 Endpoint IP 及使用方法
-    green "当前最优 Endpoint IP 结果如下，并已保存至 result.csv中："
-    cat result.csv | awk -F, '$3!="timeout ms" {print} ' | sort -t, -nk2 -nk3 | uniq | head -11 | awk -F, '{print "端点 "$1" 丢包率 "$2" 平均延迟 "$3}'
+    # Display the top ten optimal endpoint IPs and usage details
+    green "Current optimal endpoint IP results are as follows:"
+    cat result.csv | awk -F, '$3!="timeout ms" {print} ' | sort -t, -nk2 -nk3 | uniq | head -11 | awk -F, '{print "Endpoint "$1" Loss "$2" Avg delay "$3}'
     echo ""
-    yellow "使用方法如下："
-    yellow "1. 将 WireGuard 节点的默认的 Endpoint IP：engage.cloudflareclient.com:2408 替换成本地网络最优的 Endpoint IP"
+    yellow "Usage details are as follows:"
+    yellow "1. Replace the default endpoint IP of the WireGuard node: engage.cloudflareclient.com:2408, with the locally optimal endpoint IP"
 
-    # 删除 WARP Endpoint IP 优选工具及其附属文件
-    rm -f warp ip.txt
+    # Delete the WARP endpoint IP selection tool and related files
+    rm -f warp ip.txt result.csv
 }
 
+# Function to generate a list of optimal WARP IPv4 endpoint IP ranges
 endpoint4(){
-    # 生成优选 WARP IPv4 Endpoint IP 段列表
     n=0
-    iplist=100
+    iplist=200
+    temp=()
     while true; do
         temp[$n]=$(echo 162.159.192.$(($RANDOM % 256)))
         n=$(($n + 1))
@@ -134,75 +137,72 @@ endpoint4(){
         fi
     done
 
-    # 将生成的 IP 段列表放到 ip.txt 里，待程序优选
+    # Put the generated IP range list into ip.txt for program to select
     echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u > ip.txt
 
-    # 启动优选程序
+    # Start the selection program
     endpointyx
 }
 
+# Function to generate a list of optimal WARP IPv6 endpoint IP ranges
 endpoint6(){
-    # 生成优选 WARP IPv6 Endpoint IP 段列表
     n=0
-    iplist=100
+    iplist=200
+    temp=()
     while true; do
+        temp[$n]=$(echo [2606:4700:d1::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
+		n=$(($n + 1))
+if [ $n -ge $iplist ]; then
+    break
+fi
+done
+while true; do
+    if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+        break
+    else
         temp[$n]=$(echo [2606:4700:d0::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
         n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
+    fi
+    if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+        break
+    else
         temp[$n]=$(echo [2606:4700:d1::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
         n=$(($n + 1))
-        if [ $n -ge $iplist ]; then
-            break
-        fi
-    done
-    while true; do
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo [2606:4700:d0::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
-            n=$(($n + 1))
-        fi
-        if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
-            break
-        else
-            temp[$n]=$(echo [2606:4700:d1::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
-            n=$(($n + 1))
-        fi
-    done
+    fi
+done
 
-    # 将生成的 IP 段列表放到 ip.txt 里，待程序优选
-    echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u > ip.txt
+# Put the generated IP range list into ip.txt for program optimization.
+echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u > ip.txt
 
-    # 启动优选程序
-    endpointyx
+# Start optimization program.
+endpointyx
 }
 
 menu(){
-    clear
-    echo "#############################################################"
-    echo -e "#               ${RED}WARP Endpoint IP 一键优选脚本${PLAIN}               #"
-    echo -e "# ${GREEN}作者${PLAIN}: MisakaNo の 小破站                                  #"
-    echo -e "# ${GREEN}博客${PLAIN}: https://blog.misaka.rest                            #"
-    echo -e "# ${GREEN}GitHub 项目${PLAIN}: https://github.com/Misaka-blog               #"
-    echo -e "# ${GREEN}GitLab 项目${PLAIN}: https://gitlab.com/Misaka-blog               #"
-    echo -e "# ${GREEN}Telegram 频道${PLAIN}: https://t.me/misakanocchannel              #"
-    echo -e "# ${GREEN}Telegram 群组${PLAIN}: https://t.me/misakanoc                     #"
-    echo -e "# ${GREEN}YouTube 频道${PLAIN}: https://www.youtube.com/@misaka-blog        #"
-    echo "#############################################################"
-    echo ""
-    echo -e " ${GREEN}1.${PLAIN} WARP IPv4 Endpoint IP 优选 ${YELLOW}(默认)${PLAIN}"
-    echo -e " ${GREEN}2.${PLAIN} WARP IPv6 Endpoint IP 优选"
-    echo " -------------"
-    echo -e " ${GREEN}0.${PLAIN} 退出脚本"
-    echo ""
-    read -rp "请输入选项 [0-2]: " menuInput
-    case $menuInput in
-        2 ) endpoint6 ;;
-        0 ) exit 1 ;;
-        * ) endpoint4 ;;
-    esac
+clear
+echo "######################################################################################"
+echo -e "#        ${RED}WARP Endpoint IP One-Click Optimization Script for Mac${PLAIN}        #"
+echo -e "# ${GREEN}Author${PLAIN}: MisakaNo's Little Breaking Station                         #"
+echo -e "# ${GREEN}Translator${PLAIN}: YeBeKhe - https://twitter.com/yebekhe                  #"
+echo -e "# ${GREEN}Blog${PLAIN}: https://blog.misaka.rest                                     #"
+echo -e "# ${GREEN}GitHub Project${PLAIN}: https://github.com/Misaka-blog                     #"
+echo -e "# ${GREEN}GitLab Project${PLAIN}: https://gitlab.com/Misaka-blog                     #"
+echo -e "# ${GREEN}Telegram Channel${PLAIN}: https://t.me/misakanocchannel                    #"
+echo -e "# ${GREEN}Telegram Group${PLAIN}: https://t.me/misakanoc                             #"
+echo -e "# ${GREEN}YouTube Channel${PLAIN}: https://www.youtube.com/@misaka-blog              #"
+echo "######################################################################################"
+echo ""
+echo -e " ${GREEN}1.${PLAIN} WARP IPv4 Endpoint IP Optimization ${YELLOW}(Default)${PLAIN}"
+echo -e " ${GREEN}2.${PLAIN} WARP IPv6 Endpoint IP Optimization"
+echo " -------------"
+echo -e " ${GREEN}0.${PLAIN} Exit Script"
+echo ""
+read -rp "Please enter an option [0-2]: " menuInput
+case $menuInput in
+    2 ) endpoint6 ;;
+    0 ) exit 1 ;;
+    * ) endpoint4 ;;
+esac
 }
 
 menu
